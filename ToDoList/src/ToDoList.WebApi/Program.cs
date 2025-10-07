1be0dfaf2d4a4b218c11359d79c31f52a833c681
Zuzana Parýzková <92799499+zuzana-p@users.noreply.github.com>
2025-10-07 21:37:06 +0200

    prace z lekce 3

--- ToDoList/src/ToDoList.WebApi/Program.cs
+++ ToDoList/src/ToDoList.WebApi/Program.cs
@@ -1,11 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
-{
-    builder.Services.AddControllers();
-}
-
 var app = builder.Build();
-{
-    app.MapControllers();
-}
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/test", () => "This is a test!");
+
+app.MapGet("/pozdrav/{jmeno}", (string jmeno) => $"Ahoj {jmeno}!");
+app.MapGet("/secti/{a:int}/{b:int}", (int a, int b) => $"Vysledek {a} + {b} = {a + b}");
+
+app.MapGet("/nazdarSvete", () => "Nazdar Světe!");
 
 app.Run();
